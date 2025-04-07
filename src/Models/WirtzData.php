663a7f5fda255f091159ce181a922730ad264706
RA <rgoodie@users.noreply.github.com>
2025-04-07 15:26:53 -0500

    name search

--- src/Models/WirtzData.php
+++ src/Models/WirtzData.php
@@ -17,13 +17,18 @@ class WirtzData
                 $data[] = $row;
             }
             fclose($fileHandle); // Don't forget to close the file
-            $this->data = $data;
+            $this->data = $this->convertToArrayWithColumnHeaders($data);
         } else {
             // TODO: Handle error
         }
     }
 
 
+    /**
+     * Returns the path of the latest CSV file in the specified folder.
+     *
+     * @return string The path of the latest CSV file.
+     */
     public function latestFile()
     {
 
@@ -41,10 +46,16 @@ class WirtzData
     }
 
 
+    /**
+     * Returns the data as an array with column headers, where the first row serves as the keys for a multidimensional array.
+     *
+     * @return array
+     */
     public function getData()
     {
-        return (array) $this->convertToArrayWithColumnHeaders($this->data);
+        return (array) $this->data;
     }
+
 
 
     /**
@@ -61,6 +72,13 @@ class WirtzData
         foreach ($data as $row) {
             $tempArray = [];
             foreach ($headers as $key => $header) {
+                if ($header == 'First name') {
+                    $header = 'First';
+                } elseif ($header == 'Last name') {
+                    $header = 'Last';
+                } elseif ($header == 'Graduation Year') {
+                    $header = 'Grad';
+                }
                 $tempArray[$header] = $row[$key];
             }
             $result[] = $tempArray; // Add each row to the result array
@@ -68,22 +86,69 @@ class WirtzData
 
         return $result;
     }
-    
 
-    private function getUniqueColumn($column) {
-        $uniqueValues = array_values(array_unique(array_column($this->getData(), $column)));
+
+    /**
+     * Get an array of unique values for a given column.
+     *
+     * @param string $column The name of the column to get unique values for
+     * @return array An array of unique values in ascending order
+     */
+    private function getUniqueColumn($column)
+    {
+        $uniqueValues = array_values(array_unique($this->getColumnValues($column)));
         asort($uniqueValues); // or arsort for reverse sort
-        return $uniqueValues;    
+        return $uniqueValues;
+    }
+
+    /**
+     * Get the column values for a given column.
+     *
+     * @param string $column The name of the column to get values for
+     * @return array An array of values in the given column
+     */
+    private function getColumnValues($column)
+    {
+        return array_column($this->getData(), $column);
     }
 
 
-    public function getUniqueYears() {
+    public function getUniqueYears()
+    {
         return $this->getUniqueColumn('Year');
     }
 
-    public function getUniqueProductions() {
+    public function getUniqueProductions()
+    {
         return $this->getUniqueColumn('Production');
     }
 
+    public function getUniqueTeams()
+    {
+        return $this->getUniqueColumn('Team');
+    }
 
+    public function getUniqueRoles()
+    {
+        return $this->getUniqueColumn('Role');
+    }
+
+    public function getRoles()
+    {
+        return $this->getColumnValues('Role');
+    }
+
+
+
+    public function doSearch($first, $last)
+    {
+        $first = strtolower($first);
+        $last = strtolower($last);
+
+        $result = array_filter($this->getData(), function ($row) use ($first, $last) {
+            return (stripos($row['First'], $first) !== false && stripos($row['Last'], $last) !== false);
+        });
+
+        return array_values($result); // Re-index the array
+    }
 }
--- src/WirtzShow.php
+++ src/WirtzShow.php
@@ -30,9 +30,40 @@ class WirtzShow
 
     public function startpoint()
     {
-        dump($this->wirtz_data->getUniqueYears());
-        dump($this->wirtz_data->getUniqueProductions());
-        dump(    $this->wirtz_data->getData());
-        
+        // dump($this->wirtz_data->getUniqueYears());
+        // dump($this->wirtz_data->getUniqueProductions());
+        // dump($this->wirtz_data->getData());
+
+        if (is_front_page() || is_home()) {
+            // If on the front page or home page, return an empty string
+            return '';
+        }
+
+
+
+        if (isset($_GET['first']) && isset($_GET['last'])) {
+            $first = wp_kses(trim($_GET['first']), []);
+            $last = wp_kses(trim($_GET['last']), []);
+            dump([$first, $last]);
+
+            $people = $this->wirtz_data->doSearch(
+                $first,
+                $last
+            );
+            dump($people);
+
+        } else {
+            $first = 0;
+            $last = 0;
+            $people = [];
+        }
+
+        return $this->twig->render(
+            'startpoint.html.twig',
+            [
+                'people' => $people,
+                'returnPage' => $currentUrl = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+            ]
+        );
     }
 }
