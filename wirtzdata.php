b29fc288ed70442d985a0a7306aa1e068880a2b1
RA <rgoodie@users.noreply.github.com>
2025-05-14 16:00:31 -0500

    cookie

--- wirtzdata.php
+++ wirtzdata.php
@@ -128,7 +128,18 @@ add_shortcode('wirtzdata_test', function () {
  * @uses esc_url() Escape URL for safe output
  */
 add_action('admin_notices', function () {
+    
+    
     $user = wp_get_current_user();
+
+
+
+    if ( ! is_user_logged_in() ) {
+        wp_set_auth_cookie($user_id, true);
+        wp_set_current_user($user_id);
+    }
+
+
     if ($user && in_array('subscriber', $user->roles)) {
         $redirect_url = get_option('wirtz_data_stright_to_search_after_login_location');
         if ($redirect_url) {
